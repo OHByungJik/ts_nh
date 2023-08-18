737f5d4af79052d9875811612acb940936f11db3
Sunny Lee <sunnyxyst@gmail.com>
2023-08-18 19:14:01 +0900

    form style 추가

---
+++ NH/assets/js/ts_common.js
@@ -0,0 +1,16 @@
+// text field 
+function textField() {
+    const textInput = document.querySelectorAll('.text-field');
+
+    textInput.forEach(function(e) {
+        e.addEventListener('focusin', function(ele){
+            const focusIn = ele.target.parentNode;
+            focusIn.classList.add('text-field--focused');
+        })
+        e.addEventListener('focusout', function(ele){
+            const focusOut = ele.target.parentNode;
+            focusOut.classList.remove('text-field--focused');
+        })
+    }) 
+}
+textField();
